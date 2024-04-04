dc61861067976740f31be43f27b3f3a4feb9fad8
Mark Hayden <hi@markhayden.me>
2024-04-04 17:52:34 -0600

    some old stuff

--- src/pages/options/index.tsx
+++ src/pages/options/index.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Options from '@pages/options/Options';
 import '@pages/options/index.css';
-import refreshOnUpdate from 'virtual:reload-on-update-in-view';
+// import refreshOnUpdate from 'virtual:reload-on-update-in-view';
 
-refreshOnUpdate('pages/options');
+// refreshOnUpdate('pages/options');
 
 function init() {
   const appContainer = document.querySelector('#app-container');
