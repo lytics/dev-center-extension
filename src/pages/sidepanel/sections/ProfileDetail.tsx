4481557da0835ed21875e2b4ffca6e4f0fd6a609
onkarj-47 <onkar.jaliminche@contentstack.com>
2025-10-17 18:51:56 +0530

    style: add tagConfig panel in profile details

--- src/pages/sidepanel/sections/ProfileDetail.tsx
+++ src/pages/sidepanel/sections/ProfileDetail.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
+import { Box } from '@mui/material';
+import { styled } from '@mui/material/styles';
 import TreeDisplay from '@root/src/pages/sidepanel/components/TreeDisplay';
 
 interface ProfileDetailTabProps {
   profile: any;
 }
 
+const StyledPanel = styled(Box)(({ theme }) => ({
+  width: '100%',
+  backgroundColor: '#272728', // Dark background (appColors.common.darkPanel)
+  borderTopLeftRadius: theme.spacing(1), // 8px - rounded top-left corner
+  borderTopRightRadius: theme.spacing(1), // 8px - rounded top-right corner
+  borderBottomLeftRadius: 0, // Sharp bottom-left corner
+  borderBottomRightRadius: 0, // Sharp bottom-right corner
+  padding: '0.625rem', // 10px inner padding
+  cursor: 'default',
+  transition: 'none',
+  '&:hover': {
+    boxShadow: 'none',
+    transform: 'none',
+  },
+}));
+
 const ProfileDetail: React.FC<ProfileDetailTabProps> = ({ profile }) => {
-  return <TreeDisplay data={profile?.data} collapsed={2} />;
+  return (
+    <StyledPanel>
+      <TreeDisplay data={profile?.data} collapsed={2} />
+    </StyledPanel>
+  );
 };
 
 export default ProfileDetail;
