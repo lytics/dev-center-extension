d1050bb3aca1e55afb819644b1e415e01d8aa51e
onkarj-47 <onkar.jaliminche@contentstack.com>
2025-10-30 20:36:38 +0530

    style: fix PR comments

--- src/pages/sidepanel/components/EmptyState.tsx
+++ src/pages/sidepanel/components/EmptyState.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Box, Stack } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import { GeneralEmpty, ListeningEmpty } from '@root/src/pages/sidepanel/assets/svg/empty';
+import { Sparkles } from '@root/src/pages/sidepanel/assets/svg/sparkles';
+import { appColors } from '@root/src/theme/palette';
 
 type EmptyType = 'empty' | 'listening' | 'deprecated';
 
@@ -20,33 +22,20 @@ const DeprecatedContainer = styled(Stack)(() => ({
   alignItems: 'center',
 }));
 
-const SparkleImage = styled('img')(() => ({
-  width: '3.125rem', // 50px
-  height: '3.125rem', // 50px
-}));
-
 const DeprecatedText = styled(Box)(() => ({
   width: '12.9375rem', // 207px
   height: '2.375rem', // 38px
-  fontFamily: 'SF Pro, -apple-system, BlinkMacSystemFont, sans-serif',
-  fontWeight: 700,
-  fontSize: '1rem', // 16px
-  lineHeight: '100%',
-  letterSpacing: '-0.5px',
+  font: '700 1rem/1 SF Pro, sans-serif',
   textAlign: 'center',
-  color: '#383838',
+  color: appColors.neutral[600],
 }));
 
 const SecondaryText = styled(Box)(() => ({
   width: '12.9375rem', // 207px
   height: '2.125rem', // 34px
-  fontFamily: 'SF Pro, -apple-system, BlinkMacSystemFont, sans-serif',
-  fontWeight: 510,
-  fontSize: '0.875rem', // 14px
-  lineHeight: '100%',
-  letterSpacing: '-0.5px',
+  font: '500 0.875rem/1 SF Pro, sans-serif',
   textAlign: 'center',
-  color: '#8F8D8D',
+  color: appColors.neutral[400],
 }));
 
 const EmptyState: React.FC<EmptyStateProps> = ({ body, type }) => {
@@ -55,7 +44,11 @@ const EmptyState: React.FC<EmptyStateProps> = ({ body, type }) => {
       case 'listening':
         return <ListeningEmpty />;
       case 'deprecated':
-        return <SparkleImage src="/sparkles.png" alt="No legacy campaigns" />;
+        return (
+          <Box width={'3.125rem'} height={'3.125rem'}>
+            <Sparkles />
+          </Box>
+        );
       default:
         return <GeneralEmpty />;
     }
